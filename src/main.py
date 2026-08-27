86dddf59b9277bc23809e5ed7fccdac60809b8dd
alreyes26 <alreyes245@gmail.com>
2026-08-27 15:46:31 +0000

    update main.py

---
+++ src/main.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+class main:
+    
+    
+   def func(self):
+       
+       new_arr = np.array(input("Enter your numbers separated by commas: ").split(','), dtype=int)
+       arr = np.array([])
+       
+       for spot in new_arr:
+           
+           if(spot < 0 or spot > 100):
+               print("Invalid number, please enter a number between 0 and 100.")
+               return
+           else:
+              arr = np.append(arr, spot)
+              print(arr)
+              
+              
+        
+              
+    
+    
+calculation = main()
+
+result = calculation.func()
+print(result)
+
+
+    
